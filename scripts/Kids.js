7e70f7841fe217645a1120124d220fecadee4d64
James Freeman <jamesfreeman114@gmail.com>
2025-09-06 12:00:06 -0500

    Debugged original codd and added event listeners

--- scripts/Kids.js
+++ scripts/Kids.js
@@ -2,14 +2,36 @@ import { getChildren } from "./database.js"
 
 const children = getChildren()
 
+
 export const Kids = () => {
+
     let html = "<ol>"
 
     for (const child of children) {
-        html += `<li data-id="${child.id}" data-type="child" data-wish="${child.wish}">${child,name}</li>`
+        html += `<li data-id="${child.id}" 
+                 data-type="child" 
+                 data-wish="${child.wish}">${child.name}</li>`
     }
 
     html += "</ol>"
     return html
 }
 
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+    const childClickEvent = clickEvent.target
+
+    if (childClickEvent.dataset.type === "child") {
+        let childId = childClickEvent.dataset.id
+
+        for (const kid of children) {
+            if (kid.id === parseInt(childId))
+
+            
+        window.alert(`${kid.name}'s wish is ${kid.wish}`)
+        }    
+
+    }
+}
+)
